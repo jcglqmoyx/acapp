85720454f9a03f5ba11cc00cce41858201c1af53
Chintsai Hwo <jcglqmoyx@126.com>
2022-02-18 22:12:49 +0800

    create login and register module

--- static/js/src/playground/player/zbase.js
+++ static/js/src/playground/player/zbase.js
@@ -20,6 +20,11 @@ class Player extends AcGameObject {
 		this.spent_time = 0;
 
 		this.cur_skill = null;
+        
+        if (this.is_me) {
+            this.img = new Image();
+            this.img.src = this.playground.root.settings.photo;
+        }
 	}
 
 	start() {
@@ -136,18 +141,28 @@ class Player extends AcGameObject {
 		this.render();
 	}
 
-	render() {
-		this.ctx.beginPath();
-		this.ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
-		this.ctx.fillStyle = this.color;
-		this.ctx.fill();
-	}
+    render() {
+        if (this.is_me) {
+            this.ctx.save();
+            this.ctx.beginPath();
+            this.ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+            this.ctx.stroke();
+            this.ctx.clip();
+            this.ctx.drawImage(this.img, this.x - this.radius, this.y - this.radius, this.radius * 2, this.radius * 2); 
+            this.ctx.restore();
+        } else {
+            this.ctx.beginPath();
+            this.ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+            this.ctx.fillStyle = this.color;
+            this.ctx.fill();
+        }
+    }
 
-	on_destroy() {
-		for (let i = 0; i < this.playground.players.length; i++) {
-			if (this.playground.players[i] === this) {
-				this.playground.players.splice(i, 1);
-			}
-		}
-	}
+    on_destroy() {
+        for (let i = 0; i < this.playground.players.length; i++) {
+            if (this.playground.players[i] === this) {
+                this.playground.players.splice(i, 1);
+            }
+        }
+    }
 }
